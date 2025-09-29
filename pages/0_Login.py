e845d9d2013f25dbe75a3e49335b2479b7462429
Martin Ricardo Avalos Mendoza <martinquetza@gmail.com>
2025-09-28 21:24:01 -0600

    actualizacion

--- pages/0_Login.py
+++ pages/0_Login.py
@@ -130,7 +130,7 @@ with tab_coach:
     with st.form("form_admin_login", clear_on_submit=False):
         a_user = st.text_input("Usuario", value="carmen", disabled=True, key="admin_user")
         a_pass = st.text_input("Contraseña", type="password", key="admin_pass")
-        enviar_admin = st.form_submit_button("Entrar como Coach", use_container_width=True)
+        enviar_admin = st.form_submit_button("Entrar como Coach")
     if enviar_admin:
         if is_admin_ok(a_user, a_pass):
             st.session_state.role = "admin"
@@ -152,7 +152,7 @@ with tab_pac:
         with st.form("form_pac_login", clear_on_submit=False):
             tel_login = st.text_input("Teléfono", key="pac_tel_login")
             pw_login  = st.text_input("Contraseña", type="password", key="pac_pw_login")
-            enviar_login = st.form_submit_button("Entrar", use_container_width=True)
+            enviar_login = st.form_submit_button("Entrar")
         if enviar_login:
             user = login_paciente(tel_login, pw_login)
             if user:
@@ -171,7 +171,7 @@ with tab_pac:
             tel_reg = st.text_input("Teléfono", key="pac_reg_tel")
             pw1     = st.text_input("Contraseña", type="password", key="pac_reg_pw1")
             pw2     = st.text_input("Repite tu contraseña", type="password", key="pac_reg_pw2")
-            enviar_reg = st.form_submit_button("Registrarme", use_container_width=True)
+            enviar_reg = st.form_submit_button("Registrarme")
 
         if enviar_reg:
             if not (nombre.strip() and tel_reg.strip() and pw1 and pw2):
